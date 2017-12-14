7323a50fe136fa65a5ed738c88dba5d2248c7ff1
thekeygithub <33211781+thekeygithub@users.noreply.github.com>
2017-12-14 13:37:08 +0800

    Update PicturesController.java

--- src/main/java/com/ts/controller/information/pictures/PicturesController.java
+++ src/main/java/com/ts/controller/information/pictures/PicturesController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Map;     
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
